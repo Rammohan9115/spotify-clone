36a5d54af3d41299ec4171889f097dc0222e5219
Rammohan9115 <rammohan.rameez@gmail.com>
2023-09-06 12:37:03 -0400

    Song and image upload completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,10 @@ import {HiHome} from "react-icons/hi"
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast/headless";
 interface HeaderProps{
     children: React.ReactNode;
     className?: string;
@@ -15,9 +19,22 @@ const Header: React.FC<HeaderProps> = (
 ) => {
     const router = useRouter();
     const AuthModal = useAuthModal();
+    const supabaseClient = useSupabaseClient();
+    const {user} = useUser();
 
-    const handleLogout = () => {
-        //handle logout
+
+    const handleLogout = async () => {
+        const {error} = await supabaseClient.auth.signOut();
+        //reset if any song is playing
+        router.refresh();
+
+        if(error){
+            toast.error(error.message);
+        }else
+        {
+            toast.success('Logged out!');
+        }
+
     }
     return ( <div className={twMerge(`
     h-fit
@@ -57,22 +74,34 @@ const Header: React.FC<HeaderProps> = (
         </div>
 
         <div className="flex justify-between items-center gap-x-4">
-            <>
-            <div>
-                <Button onClick={AuthModal.onOpen} className="
-                 bg-transparent text-neutral-300 font-medium
-                ">
-                    Sign up
+            {user ? (
+                <div className="flex gap-x-4 items-center">
+                <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                  Logout
                 </Button>
-            </div>
-            <div>
-                <Button onClick={AuthModal.onOpen} className="
-                 bg-white px-6 py-2
-                ">
-                    Log in
+                <Button onClick={() => router.push('/account')} className="bg-white">
+                  <FaUserAlt />
                 </Button>
-            </div>
-            </>
+              </div>
+              
+            ):(
+                    <>
+                    <div>
+                        <Button onClick={AuthModal.onOpen} className="
+                        bg-transparent text-neutral-300 font-medium
+                        ">
+                            Sign up
+                        </Button>
+                    </div>
+                    <div>
+                        <Button onClick={AuthModal.onOpen} className="
+                        bg-white px-6 py-2
+                        ">
+                            Log in
+                        </Button>
+                    </div>
+                    </>
+            )}
         </div>
      
     </div>
--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,21 @@
 "use client";
 import {TbPlaylist} from "react-icons/tb"
 import {AiOutlinePlus} from "react-icons/ai"
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 const Library = () => {
+    const AuthModal = useAuthModal();
+    const UploadModal = useUploadModal();
+    
+    const {user,subscription} = useUser(); 
     const onClick = () => {
-        //upload model
-    };
+            if(!user)
+            {
+                return AuthModal.onOpen();
+            }
+                return UploadModal.onOpen();
+        };
 
     return ( <div className="flex flex-col"
     >
